723a66fb86002b15df49215d37db5812d8230ff0
Leopoldo Garcia <legarbo@gmail.com>
2017-01-09 21:57:58 -0500

    add delete and update

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -1,16 +1,24 @@
 //const MongoClient = require('mongodb').MongoClient;
 const { MongoClient, ObjectID } = require('mongodb');
 
+
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
     return console.log('Unable to connect to MongoDB server');
   }
   console.log('Connected to MongoDB server');
-
-  db.collection('Todos').find().count().then((count) => {
-    console.log(`Todos count: ${count}`);
-  }, (err) => {
-    console.log('Unable to fetch todos', err);
+  
+  // findOneAndUpdate
+  db.collection('Todos').findOneAndUpdate({_id: new ObjectID("587427984a8ecfcd22e3ae3c")
+  }, {
+    $set: {
+      completed: true
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
   });
 
 //  db.close();
--- playground/mongodb-update2.js
+++ playground/mongodb-update2.js
@@ -1,16 +1,28 @@
 //const MongoClient = require('mongodb').MongoClient;
 const { MongoClient, ObjectID } = require('mongodb');
 
+
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
     return console.log('Unable to connect to MongoDB server');
   }
   console.log('Connected to MongoDB server');
-
-  db.collection('Todos').find().count().then((count) => {
-    console.log(`Todos count: ${count}`);
-  }, (err) => {
-    console.log('Unable to fetch todos', err);
+  
+  // findOneAndUpdate
+  db.collection('Users').findOneAndUpdate({_id: new ObjectID("587056f97f3e0fa241fbbe57")
+  }, {
+    $set: {
+      name: 'Leopoldo'
+    }
+    ,
+    $inc: {
+      age: 30
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
   });
 
 //  db.close();
